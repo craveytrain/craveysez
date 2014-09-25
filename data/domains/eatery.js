9ae60a7f66e0512e9d8ae7bc1c0e6b35c7384d3a
Mike Cravey <techie@craveytrain.com>
2014-09-25 16:49:13 +0200

    Moves stub data out to data dir
    Now stub data can be use by test or or hydrate the data store, which
    will help me until I get real data.

---
+++ data/domains/eatery.js
@@ -0,0 +1,6 @@
+module.exports = {
+	id: 'e23423',
+	name: 'Hot New Spot',
+	location: [ 23.234234234, -32.234234234 ],
+	address: '301 Congress Ave, Austin, TX, 78701'
+};
--- test/domain_models.js
+++ test/domain_models.js
@@ -1,44 +1,46 @@
 var assert = require( 'assert' );
 var models = require( '../models/domains' );
 var merge = require( 'merge' );
+var stubModels = require( '../data/domains' );
 
+// To make pretty titles
 function toTitleCase( str ) {
 	return str.replace( /\w\S*/g, function ( txt ) {
 		return txt.charAt( 0 ).toUpperCase() + txt.substr( 1 ).toLowerCase();
 	} );
 }
 
-var stubModels = {
-	eatery: {
-		id: 'e23423',
-		name: 'Hot New Spot',
-		location: [ 23.234234234, -32.234234234 ],
-		address: '301 Congress Ave, Austin, TX, 78701'
-	},
-	review: {
-		id: 'r2342342',
-		title: 'Good Eats',
-		eatery: 'e23423',
-		timestamp: new Date(),
-		content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-	}
-};
-
+// Hard code values we know will be transformed by the domain models
 var expectedModels = {
 	review: {
 		eatery: '{{eatery:e23423}}',
 	}
 };
 
+// For each domain model type
 Object.keys( models ).forEach( function ( modelName ) {
+	// Give it a pretty name
 	describe( toTitleCase( modelName ), function () {
 		describe( 'create()', function () {
 			it( 'should fail if data is missing in creation', function () {
+				// Catch the throw that comes from missing data
 				assert.throws(
 					function () {
 						models[ modelName ].create( {
 							id: 'test'
 						} );
+					},
+					Error
+				);
+			} );
+		} );
+
+		describe( 'get()', function () {
+			it( 'should fail if coupon has not been created', function () {
+				// catch the throw from missing coupon
+				assert.throws(
+					function () {
+						models[ modelName ].get( 'test' );
 					},
 					Error
 				);
@@ -54,17 +56,7 @@ Object.keys( models ).forEach( function ( modelName ) {
 				// check for equality
 				assert.deepEqual( models[ modelName ].get( stubModels[ modelName ].id ), expectedModel );
 			} );
-		} );
 
-		describe( 'get()', function () {
-			it( 'should fail if coupon has not been created', function () {
-				assert.throws(
-					function () {
-						models[ modelName ].get( 'test' );
-					},
-					Error
-				);
-			} );
 		} );
 	} );
 } );
